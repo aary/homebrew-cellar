22afcfa06f272896c2ec6438da0dede83ea7e74c
Aaryaman Sagar <rmn100@gmail.com>
2016-12-28 01:54:39 +0530

    Added new version

--- testbrew.rb
+++ testbrew.rb
@@ -1,7 +1,7 @@
 class Testbrew < Formula
   desc ""
   homepage ""
-  url "https://github.com/aary/testbrew/archive/v0.1.tar.gz"
+  url "https://github.com/aary/testbrew/archive/v0.2.tar.gz"
   version "0.0"
   head "https://github.com/aary/testbrew.git"
   sha256 "ebea9210ed5548b0a11cd2c0aed28788d4306e8ad5f772400e64e5f4fbc2d11b"
