543eac8ddd7dbca3b84680b4d138278712e85082
Aaryaman Sagar <rmn100@gmail.com>
2016-12-28 09:10:38 +0530

    removed old references

--- testbrew.rb
+++ testbrew.rb
@@ -1,8 +1,6 @@
 class Testbrew < Formula
   desc ""
   homepage ""
-  url "https://github.com/aary/testbrew/archive/v0.2.tar.gz"
-  version "0.0"
   head "https://github.com/aary/testbrew.git"
   sha256 "ebea9210ed5548b0a11cd2c0aed28788d4306e8ad5f772400e64e5f4fbc2d11b"
 
